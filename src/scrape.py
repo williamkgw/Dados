de507e1bfbac1b4c538ea93ac7787eeeca2f7939
William Kennedy <william@gestaopet.com.br>
2025-03-03 20:45:02 -0300

    Fixed halting problem putting a timeout in the webscraping; Changed the business rule of init_export_template;

--- src/scrape.py
+++ src/scrape.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import logging
 
-from src.extraction.webscraping import download
+from src.extraction.webscraping import download_with_timeout
 
 from src.config import ConfigLoad
 
@@ -26,7 +26,15 @@ def get_logins(emps, credentials):
 
             if extra is not np.nan:
                 vendas_clientes_dir = vendas_clientes_dir / f'{extra}'
-            try:
-                download(acesso, senha, nome_acesso, vendas_clientes_dir, date)
-            except Exception as e:
-                logging.warning(f'{emp}/Falha no Download/{e}')
+
+            sucess = download_with_timeout(acesso, senha, nome_acesso, vendas_clientes_dir, date)
+
+            if sucess:
+                logging.info(f"Download completed for {emp} - {nome_acesso}")
+            else:
+                expected_downloaded_files = [vendas_clientes_dir.joinpath(file_stem).with_suffix('.csv') for file_stem in ("Vendas", "Clientes", "Animais_e_Clientes")]
+                for downloaded_file in expected_downloaded_files:
+                    downloaded_file.unlink(missing_ok = True)
+
+                logging.warning(f"Download failed for {emp} - {nome_acesso}")
+            
