0d2971e4a0441ce2201b798a16515087d6176cf6
William Kennedy <william@gestaopet.com.br>
2025-03-09 18:29:21 -0300

    Added logger support for getting past mappings and import template;

--- src/util/others.py
+++ src/util/others.py
@@ -1,9 +1,15 @@
+import logging
 import shutil
 
 def copy_file_to(path_src, path_dest):
-    path_parent_dest_dir = path_dest.parent
-    path_parent_dest_dir.mkdir(exist_ok = True)
-    shutil.copy2(path_src, path_dest)
+    logger = logging.getLogger("src.util.others")
+
+    try:
+        path_parent_dest_dir = path_dest.parent
+        path_parent_dest_dir.mkdir(exist_ok = True)
+        shutil.copy2(path_src, path_dest)
+    except Exception as e:
+        logger.exception(f"Exception during copy_file_to(path_src = {path_src}, path_dest = {path_dest})")
 
 def copy_dir_to_recursively(path_dir_src, path_dir_dest):
     shutil.copytree(path_dir_src, path_dir_dest, dirs_exist_ok = True)
