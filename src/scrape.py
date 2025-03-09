2f56628ca78016596fe35c5b3183411aea392579
William Kennedy <william@gestaopet.com.br>
2025-03-09 14:35:40 -0300

    Corrected logger naming, UTC timestamp; Clean webscraping from printting and configured to logging instead;

--- src/scrape.py
+++ src/scrape.py
@@ -8,14 +8,20 @@ from src.config import ConfigLoad
 def get_logins(emps, credentials):
     import numpy as np
 
+    logger = logging.getLogger('src.scrape')
+
     credentials_df = pd.read_excel(credentials)
     credentials_df = credentials_df[credentials_df['SITES'] == 'SIMPLESVET']
     for emp in emps:
+        logger.info(f"Starting scrape for {emp}")
         config_company = ConfigLoad('end', emp)
 
         credentials_filtered_df = credentials_df[credentials_df['EMPRESA'] == emp]
+
+        if credentials_filtered_df.empty:
+            logger.warning(f"No credentials found for {emp}")
+
         for _, row in credentials_filtered_df.iterrows():
-            print(emp)
             date = config_company.date
             vendas_clientes_dir = config_company.input_dir.cargas.carga_company.dir_name
 
@@ -30,11 +36,11 @@ def get_logins(emps, credentials):
             sucess = download_with_timeout(acesso, senha, nome_acesso, vendas_clientes_dir, date)
 
             if sucess:
-                logging.info(f"Download completed for {emp} - {nome_acesso}")
+                logger.info(f"Download completed for {emp} - {nome_acesso}")
             else:
                 expected_downloaded_files = [vendas_clientes_dir.joinpath(file_stem).with_suffix('.csv') for file_stem in ("Vendas", "Clientes", "Animais_e_Clientes")]
                 for downloaded_file in expected_downloaded_files:
                     downloaded_file.unlink(missing_ok = True)
 
-                logging.warning(f"Download failed for {emp} - {nome_acesso}")
+                logger.warning(f"Download failed for {emp} - {nome_acesso}")
             
